79e26eecf03fa3bd776afb9c5e7f69ad08b3964c
keynold <nqphu1998@gmail.com>
2025-10-31 23:08:43 +0700

    add nix-homebrew

--- darwin/default.nix
+++ darwin/default.nix
@@ -1,7 +1,23 @@
-{ self, nix-darwin, home-manager, agenix, configuration, inputs, ... }:
+{ self, nix-darwin, home-manager, agenix, configuration, inputs,nix-homebrew, ... }:
 {
   darwinConfigurations."dev" = nix-darwin.lib.darwinSystem {
     modules = [
+      nix-homebrew.darwinModules.nix-homebrew
+      {
+        nix-homebrew = {
+          # Install Homebrew under the default prefix
+          enable = true;
+
+          # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+          enableRosetta = true;
+
+          # User owning the Homebrew prefix
+          user = "dev";
+
+          # Automatically migrate existing Homebrew installations
+          autoMigrate = true;
+        };
+      }
       ../modules/apps.nix
       ../modules/homebrew
       ../modules/user.nix
